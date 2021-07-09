10a40b2075d5ad41c13b599a5167167916d796ac
常洧丞 <s0975247623@gmail.com>
2021-07-10 00:31:46 +0800

    An easy website about myself

---
+++ server.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask import render_template
+from flask import url_for
+app=Flask(__name__)
+@app.route("/")
+def hello():
+    return render_template("richardo.html")
+@app.route("/About_me")
+def hello2():
+    return render_template("About_me.html")
+if __name__=="__main__":
+    app.debug=True
+    app.run(host='192.168.187.28', port=5000)
