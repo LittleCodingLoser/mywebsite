b18e919dc7462da6cd166876a9cbd168e93d8fc4
LittleCodingLoser <s0975247623@gmail.com>
2022-02-04 18:47:31 +0800

    Overall layout update

--- server.py
+++ server.py
@@ -4,11 +4,11 @@ from flask import url_for
 app=Flask(__name__)
 @app.route("/")
 def hello():
-    return render_template("richardo.html")
-@app.route("/About_me")
+    return render_template("richardo.html") 
+@app.route("/About")
 def hello2():
     return render_template("About_me.html")
-@app.route("/My_GitHub")
+@app.route("/Portfolio")
 def hello3():
     return render_template("MyGitHub.html")
 if __name__=="__main__":
